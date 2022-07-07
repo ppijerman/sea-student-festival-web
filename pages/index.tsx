b8d1492dc0c39f9f0f2345eb236285452e234383
Yudhistira Arief Wibowo <yudhistira.wibowo@itsmeyaw.id>
2022-07-07 20:06:50 +0200

    Removed unused component and added consistency

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Countdown from "./components/countdown";
+import Countdown from "./components/countdown"
 
 const Home: NextPage = () => {
   return (
