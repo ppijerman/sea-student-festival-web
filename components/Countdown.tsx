9621dc56ade7ba5b905c899fe5685d26b4f7c2db
Corliansa Kusumah <corliansa@gmail.com>
2022-07-11 21:57:50 +0200

    fixed styledcomponent

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -1,45 +1,45 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import Button from "./Button";
+import Button from './Button';
 
 const CountdownWrapper = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 100%;
   margin: 0 20px;
-  `;
+`;
 
 const CountdownFrame = styled.div`
-    align-self: center;
-    background-color: var(--blue);
-    padding: 40px;
-    min-height: 300px;
-    max-width: 100%;
-    width: max-content;
-    border-radius: 30px;
-    border: solid 8px var(--sand);
-    color: var(--yellow);
-    gap: 20px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  `;
+  align-self: center;
+  background-color: var(--blue);
+  padding: 40px;
+  min-height: 300px;
+  max-width: 100%;
+  width: max-content;
+  border-radius: 30px;
+  border: solid 8px var(--sand);
+  color: var(--yellow);
+  gap: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 const CountdownBody = styled.div`
-    display: flex;
-    flex-wrap: nowrap;
-    flex-direction: row;
-    justify-content: center;
-    margin-bottom: 25px;
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: row;
+  justify-content: center;
+  margin-bottom: 25px;
 
-    @media only screen and (max-width: 992px) {
-      flex-direction: column;
-      align-self: center;
-    }
-  `;
+  @media only screen and (max-width: 992px) {
+    flex-direction: column;
+    align-self: center;
+  }
+`;
 
 const CountdownTitle = styled.div`
-  font-size: 48px;
+  font-size: 44px;
   text-align: center;
   font-weight: 900;
   letter-spacing: 2px;
@@ -47,56 +47,59 @@ const CountdownTitle = styled.div`
   line-height: 80%;
   margin-bottom: 20px;
   word-break: break-word;
-  `;
+  @media only screen and (min-width: 420px) {
+    font-size: 48px;
+  }
+`;
 
 const CountdownDigit = styled.div`
-    width: 200px;
-    margin-top: -20px;
-  
-    @media only screen and (max-width: 992px) {
-      margin-bottom: 3em;
-      margin-top: 0;
-    }
-  `;
+  width: 200px;
+  margin-top: -20px;
+
+  @media only screen and (max-width: 992px) {
+    margin-bottom: 3em;
+    margin-top: 0;
+  }
+`;
 
 const CountdownNumber = styled.div`
-    font-size: 150px;
-    font-weight: 600;
-    font-family: 'Piazzolla', serif;
-    padding: 0px;
-    margin: 0px;
-    line-height: 80%;
-    text-align: center;
+  font-size: 150px;
+  font-weight: 600;
+  font-family: 'Piazzolla', serif;
+  padding: 0px;
+  margin: 0px;
+  line-height: 80%;
+  text-align: center;
 
-    @media only screen and (max-width: 992px) {
-      font-size: 100px;
-    }
-  `;
+  @media only screen and (max-width: 992px) {
+    font-size: 100px;
+  }
+`;
 
 const CountdownUnit = styled.div`
-    font-size: 36px;
-    font-weight: 600;
-    font-family: 'Piazzolla', serif;
-    padding: 0px;
-    line-height: 80%;
-    text-align: center;
-    margin-top: 20px;
-  `;
+  font-size: 36px;
+  font-weight: 600;
+  font-family: 'Piazzolla', serif;
+  padding: 0px;
+  line-height: 80%;
+  text-align: center;
+  margin-top: 20px;
+`;
 
 const BuyTicketCountdownButton = styled(Button)`
-    border: solid 8px var(--sand);
-    margin-top: -37.5px;
-    width: max-content;
-    align-self: center;
+  border: solid 8px var(--sand);
+  margin-top: -37.5px;
+  width: max-content;
+  align-self: center;
 
-    @media only screen and (max-width: 992px) {
-      font-size: 16px;
-    }
-  `;
+  @media only screen and (max-width: 992px) {
+    font-size: 16px;
+  }
+`;
 
 const ButtonText = styled.span`
-    margin-right: 1em;
-  `;
+  margin-right: 1em;
+`;
 
 // @ts-ignore
 const Countdown = ({ time }) => {
@@ -136,34 +139,42 @@ const Countdown = ({ time }) => {
   };
 
   return (
-      <CountdownWrapper>
-        <CountdownFrame>
-          <CountdownTitle>
-            Opening Countdown
-          </CountdownTitle>
-          <CountdownBody>
-            <CountdownDigit>
-              <CountdownNumber>{(duration / (3600 * 24 * 1000)).toFixed(0)}</CountdownNumber>
-              <CountdownUnit>{duration / (3600 * 24 * 1000) > 1 ? 'Days' : 'Day'}</CountdownUnit>
-            </CountdownDigit>
-            <CountdownDigit>
-              <CountdownNumber>{new Date(duration).getHours()}</CountdownNumber>
-              <CountdownUnit>{new Date(duration).getHours() > 1 ? 'Hours' : 'Hour'}</CountdownUnit>
-            </CountdownDigit>
-            <CountdownDigit>
-              <CountdownNumber>{new Date(duration).getMinutes()}</CountdownNumber>
-              <CountdownUnit>{new Date(duration).getMinutes() > 1 ? 'Minutes' : 'Minute'}</CountdownUnit>
-            </CountdownDigit>
-            <CountdownDigit>
-              <CountdownNumber>{new Date(duration).getSeconds()}</CountdownNumber>
-              <CountdownUnit>{new Date(duration).getSeconds() > 1 ? 'Seconds' : 'Second'}</CountdownUnit>
-            </CountdownDigit>
-          </CountdownBody>
-        </CountdownFrame>
-        <BuyTicketCountdownButton href={'/'}>
-          <ButtonText>Grab your ticket</ButtonText> &rarr;
-        </BuyTicketCountdownButton>
-      </CountdownWrapper>
+    <CountdownWrapper>
+      <CountdownFrame>
+        <CountdownTitle>Opening Countdown</CountdownTitle>
+        <CountdownBody>
+          <CountdownDigit>
+            <CountdownNumber>
+              {(duration / (3600 * 24 * 1000)).toFixed(0)}
+            </CountdownNumber>
+            <CountdownUnit>
+              {duration / (3600 * 24 * 1000) > 1 ? 'Days' : 'Day'}
+            </CountdownUnit>
+          </CountdownDigit>
+          <CountdownDigit>
+            <CountdownNumber>{new Date(duration).getHours()}</CountdownNumber>
+            <CountdownUnit>
+              {new Date(duration).getHours() > 1 ? 'Hours' : 'Hour'}
+            </CountdownUnit>
+          </CountdownDigit>
+          <CountdownDigit>
+            <CountdownNumber>{new Date(duration).getMinutes()}</CountdownNumber>
+            <CountdownUnit>
+              {new Date(duration).getMinutes() > 1 ? 'Minutes' : 'Minute'}
+            </CountdownUnit>
+          </CountdownDigit>
+          <CountdownDigit>
+            <CountdownNumber>{new Date(duration).getSeconds()}</CountdownNumber>
+            <CountdownUnit>
+              {new Date(duration).getSeconds() > 1 ? 'Seconds' : 'Second'}
+            </CountdownUnit>
+          </CountdownDigit>
+        </CountdownBody>
+      </CountdownFrame>
+      <BuyTicketCountdownButton href={'/'}>
+        <ButtonText>Grab your ticket</ButtonText> &rarr;
+      </BuyTicketCountdownButton>
+    </CountdownWrapper>
   );
 };
 
